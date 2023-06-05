dd53f1c52a83b35e8c57c016c83450ed2e028dd8
RENBOW3 <thanapat16849@gmail.com>
2023-06-05 23:36:54 +0700

    First commit: just login, members page no typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,50 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { LoginCardComponent } from './login-card/login-card.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { MemberComponent } from './member/member.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginCardComponent,
+    MemberComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule,
+    NzCardModule,
+    NzInputModule,
+    NzLayoutModule
+
+
+    
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
